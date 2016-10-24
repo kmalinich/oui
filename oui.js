232f823a04bd90d2b7af33f4a441894c3fd169b5
kdm <kmalinich@luxotticaretail.com>
2016-10-24 18:16:54 -0400

    Simplified, cleaned up a bit. Moved from using got to request for HTTP request handling, as request supports HTTP proxy envirionment variables.

--- oui.js
+++ oui.js
@@ -7,57 +7,62 @@ process.title = "oui";
 global.Promise = require("pinkie-promise");
 
 require("get-stdin")().then(function(str) {
-  str = str.trim();
-  if (str)
-    lookup(str);
-  else
-    parseArgs(process.argv[2]);
+	str = str.trim();
+	if (str)
+		lookup(str);
+	else
+		parseArgs(process.argv[2]);
 });
 
 function parseArgs(arg) {
-  if (arg === "--update") {
-    var interval = require("char-spinner")();
-    require("./update.js")(true, function(err) {
-      clearInterval(interval);
-      if (err) process.stdout.write(err + "\n");
-      process.exit(err ? 1 : 0);
-    });
-  } else if (!arg || arg === "--help") {
-    process.stdout.write([
-      "",
-      "  Usage: oui mac [options]",
-      "",
-      "  Options:",
-      "",
-      "    --help      display this help",
-      "    --update    update the database",
-      "    --version   print the version",
-      "",
-      ""
-    ].join("\n"));
-    process.exit(0);
-  } else if (arg === "-v" || arg === "-V" || arg === "--version") {
-    process.stdout.write(require("./package.json").version + "\n");
-  } else {
-    lookup(arg);
-  }
+	if (arg === "--update") {
+		var interval = require("char-spinner")();
+
+		require("./update.js")(true, function(err) {
+			clearInterval(interval);
+
+			if (err) process.stdout.write(err + "\n");
+			process.exit(err ? 1 : 0);
+		});
+	}
+	else if (!arg || arg === "--help") {
+		process.stdout.write([
+			"",
+			"  Usage: oui mac [options]",
+			"",
+			"  Options:",
+			"",
+			"    --help      display this help",
+			"    --update    update the database",
+			"    --version   print the version",
+			"",
+			""
+		].join("\n"));
+		process.exit(0);
+	}
+	else if (arg === "-v" || arg === "-V" || arg === "--version") {
+		process.stdout.write(require("./package.json").version + "\n");
+	}
+	else {
+		lookup(arg);
+	}
 }
 
 function lookup(str) {
-  var oui = require("./");
-  var result;
+	var oui = require("./");
+	var result;
 
-  try {
-    result = oui(str);
-  } catch (err) {
-    process.stdout.write(err.message + "\n");
-    process.exit(1);
-  }
+	try {
+		result = oui(str);
+	} catch (err) {
+		process.stdout.write(err.message + "\n");
+		process.exit(1);
+	}
 
-  if (result)
-    process.stdout.write(result + "\n");
-  else
-    process.stdout.write(str + " not found in database\n");
+	if (result)
+		process.stdout.write(result + "\n");
+	else
+		process.stdout.write(str + " not found in database\n");
 
-  process.exit(0);
+	process.exit(0);
 }
--- update.js
+++ update.js
@@ -1,112 +1,72 @@
 #!/usr/bin/env node
 "use strict";
 
-var fs        = require('fs');
-var got       = require('got');
-var stringify = require('json-stable-stringify');
-var dbPath    = require('path').join(__dirname, 'db.json');
 var countries = require('country-data').countries;
-
-var http   = require('http');
-var tunnel = require('tunnel');
-var url    = require('url');
+var dbPath    = require('path').join(__dirname, 'db.json');
+var fs        = require('fs');
+var request   = require('request');
+var stringify = require('json-stable-stringify');
 
 // IEEE database URL
 var source = 'http://standards-oui.ieee.org/oui/oui.txt';
 
 module.exports = function update(isCLI, cb) {
-  var req_options = {};
+	request(source, function (error, response, body) { 
+		if (!error && response.statusCode == 200) {
+			parse(body.split("\n"), function(result) {
+				var str = stringify(result, {space: 1, cmp: function(a, b) {
+					return parseInt(a.key, 16) > parseInt(b.key, 16) ? 1 : -1;
+				}});
 
-  // If a proxy envirionment variable is detected, parse it and add the appropriate options 
-  if (process.env.proxy !== 'undefined' && process.env.proxy) {
-    var proxy_url_object = url.parse(process.env.proxy);
-
-    process.stdout.write([
-      'Using proxy host \''+proxy_url_object.hostname+'\', port \''+proxy_url_object.port+'\'.\n',
-    ].join("\n"));
-
-    req_options = {
-      host   : proxy_url_object.hostname,
-      port   : proxy_url_object.port,
-      method : 'GET',
-      path   : source // Full URL as path when using a proxy
-    };
-  }
-  else {
-    // No proxy
-    var source_url_object = url.parse(source);
-    console.log(source_url_object);
-
-    req_options = {
-      host   : source_url_object.hostname,  
-      port   : 80,
-      method : 'GET',
-      path   :  // Full URL as path when using a proxy
-    };
-  }
-
-  console.log(req_options);
-
-  // var req = http.request(req_options, function (res) {
-  // 	res.on('data', function (data) {
-  // 		console.log(data.toString());
-  // 	});
-  // });
-
-  var req = http.request(req_options).catch(cb).then(function(res) {
-    parse(res.body.split("\n"), function(result) {
-
-      var str = stringify(result, {space: 1, cmp: function(a, b) {
-        return parseInt(a.key, 16) > parseInt(b.key, 16) ? 1 : -1;
-      }});
-
-      if (!isCLI) {
-        cb(null, result);
-        fs.writeFile(dbPath, str);
-      }
-      else {
-        fs.writeFile(dbPath, str, cb);
-      }
-    });
-  });
-
-  req.end();
-}
+				if (!isCLI) {
+					cb(null, result);
+					fs.writeFile(dbPath, str);
+				}
+				else {
+					fs.writeFile(dbPath, str, cb);
+				}
+			});
+		}
+		else {
+			cb(error);
+		}
+	});
+};
 
 function isStart(firstLine, secondLine) {
-  if (firstLine === undefined || secondLine === undefined) return false;
-  return firstLine.trim().length === 0 && /([0-9A-F]{2}[-]){2}([0-9A-F]{2})/.test(secondLine);
+	if (firstLine === undefined || secondLine === undefined) return false;
+	return firstLine.trim().length === 0 && /([0-9A-F]{2}[-]){2}([0-9A-F]{2})/.test(secondLine);
 }
 
 function parse(lines, cb) {
-  var result = {}, i = 3;
+	var result = {}, i = 3;
 
-  while (i !== lines.length) {
-    if (isStart(lines[i], lines[i + 1])) {
-      var oui   = lines[i + 2].substring(0, 6).trim();
-      var owner = lines[i + 1].replace(/\((hex|base 16)\)/, "").substring(10).trim();
+	while (i !== lines.length) {
+		if (isStart(lines[i], lines[i + 1])) {
+			var oui   = lines[i + 2].substring(0, 6).trim();
+			var owner = lines[i + 1].replace(/\((hex|base 16)\)/, "").substring(10).trim();
 
-      i += 3;
-      while (!isStart(lines[i], lines[i + 1]) && i < lines.length) {
-        if (lines[i] && lines[i].trim()) owner += "\n" + lines[i].trim();
-        i++;
-      }
+			i += 3;
+			while (!isStart(lines[i], lines[i + 1]) && i < lines.length) {
+				if (lines[i] && lines[i].trim()) owner += "\n" + lines[i].trim();
+				i++;
+			}
 
-      // ensure upper case on hex digits
-      oui = oui.toUpperCase();
+			// ensure upper case on hex digits
+			oui = oui.toUpperCase();
 
-      // remove excessive whitespace
-      owner = owner.replace(/[ \t]+/gm, " ");
+			// remove excessive whitespace
+			owner = owner.replace(/[ \t]+/gm, " ");
 
-      // replace country shortcodes
-      var shortCode = (/\n([A-Z]{2})$/.exec(owner) || [])[1];
-      if (shortCode && countries[shortCode]) {
-        owner = owner.replace(/\n.+$/, "\n" + countries[shortCode].name);
-      }
+			// replace country shortcodes
+			var shortCode = (/\n([A-Z]{2})$/.exec(owner) || [])[1];
+			if (shortCode && countries[shortCode]) {
+				owner = owner.replace(/\n.+$/, "\n" + countries[shortCode].name);
+			}
 
-      result[oui] = owner;
-    }
-  }
+			result[oui] = owner;
+		}
+	}
 
-  if (cb) cb(result);
+	if (cb) cb(result);
 }
